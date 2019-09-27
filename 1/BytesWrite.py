284e03d92296d3796a170c01891c801cddf14d54
vohyz <531322336@qq.com>
2019-09-27 11:30:33 +0800

    feature - 第一部分完成，文件目录待修改， 排序算法待修改

--- 1/BytesWrite.py
+++ 1/BytesWrite.py
@@ -1,16 +1,32 @@
-import TextChange
+import re
 
-pathTxt = "C:\\Users\\msi-\\Desktop\\Pycalculate\\1\\sample.txt"     #此处sample.txt路径
-pathDat = "C:\\Users\\msi-\\Desktop\\Pycalculate\\1\\sort.dat"       #此处sort.dat路径
-def BytesWrite(pathTxt, pathDat):
-    WordListB = TextChange.Str2Bytes(pathTxt)
+def SelectWords(path):
+    f = open(path,'r')
+    WordList = []
+    for i in f:
+        WordList +=(re.split('[,/.\\\n \t\r:|]',i))
+    f.close()
+    WordList.sort(key=str.lower)
+    while WordList[0] == '':
+        del WordList[0]
+    return WordList
+
+def BytesWrite(WordList, pathDat):
+    WordListB = []
+    for i in WordList:
+        length = str(len(i))
+        word = length
+        word += i
+        WordListB.append(word.encode("utf-8")) 
+
     KBlist = WordListB[0]
     f = open(pathDat, 'w+')
     for i in WordListB[1:]:
         if len(KBlist) + len(i) > 1024:
-            print(KBlist, file= f)
+            print(KBlist, file = f, end = '')
             KBlist = i
         else:
             KBlist += i
     if len(KBlist) > 1:
-        print(KBlist, file= f)
+        print(KBlist, file = f, end = '')
+    f.close()
--- 1/main.py
+++ 1/main.py
@@ -1,22 +1,49 @@
 import BytesWrite
-import linecache  
-import random
-import logging
+import time
+import mmap
+import os
 
+pathSam = "C:\\Users\\ZYX\\Desktop\\Pycalculate\\1\\sample.txt"     #此处sample.txt路径
 pathDat = "C:\\Users\\ZYX\\Desktop\\Pycalculate\\1\\sort.dat"       #此处sort.dat路径
 pathTxt = "C:\\Users\\ZYX\\Desktop\\Pycalculate\\1\\test.txt"       #此处test.txt路径
-
+pathLog = "C:\\Users\\ZYX\\Desktop\\Pycalculate\\1\\out.log"        #此处out.log路径
 def SearchWords():
-    f = open(pathTxt,'r')
+    t = open(pathTxt,'r')
+    l = open(pathLog,'w+')
+    fd = os.open(pathDat, os.O_RDWR)
+    m = mmap.mmap(fd, 0,  access= mmap.ACCESS_READ)
     wList = []
-    for i in f:
-        wList.append(i)
-    while True:
-        a = random.randrange(1, 168)
-        theline = linecache.getline(pathDat, a)
-        for i in wList:
-            if i in theline:
-                
+    for i in t:
+        wList += i.split()
+    for i in range(0, len(wList)):
+        findNo = m.find(wList[i].encode("utf-8"))
+        if findNo == -1:
+            print("notfound", file= l)
+            continue
+        findLength = len(wList[i])
+        site = findNo + findLength
+        rstLength = ''
+        while chr(m[site]) in '0123456789':
+            rstLength += str(chr(m[site]))
+            site += 1
+        print(m[site:(site+int(rstLength))].decode(), file= l)
+    t.close()
+    l.close()
 
-    logging.basicConfig(filename='out.log', level=logging.INFO)
-    logging.debug('debug message')
+
+if __name__ == "__main__":
+    wordList = BytesWrite.SelectWords(pathSam)
+
+    startTime = time.time()
+    BytesWrite.BytesWrite(wordList, pathDat)
+    endTime = time.time()
+    costTime1 = endTime - startTime
+
+    startTime = time.time()
+    SearchWords()
+    endTime = time.time()
+    costTime2 = endTime - startTime
+    l = open(pathLog,'a+')
+    print(costTime1, file= l)
+    print(costTime2, file= l)
+    l.close()
