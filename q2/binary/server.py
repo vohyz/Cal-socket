e0174be9ab7f4f6f9f6a693b5cb510d7ce178a0a
vohyz <531322336@qq.com>
2019-10-11 10:04:51 +0800

    feature - q2完善

--- q2/binary/server.py
+++ q2/binary/server.py
@@ -1,5 +1,6 @@
 import mmap
 import os
+import time
 import BytesWrite
 import socketserver
 import socket
@@ -15,6 +16,55 @@ pathDat = module_path + "out\\sort.dat"     #此处sort.dat路径
 pathLog = module_path + "out\\out.log"      #此处out.log路径
 
 ip_port=("127.0.0.1",8000)
+g_conn_pool = []
+thread_max = 5
+
+def init():
+    """
+    初始化服务端
+    """
+    global g_socket_server
+    g_socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 创建 socket 对象
+    g_socket_server.bind(ip_port)
+    g_socket_server.listen(5)  # 最大等待数
+    print("服务端已启动，等待客户端连接...")
+    
+
+def accept_client():
+    """
+    接收新连接
+    """
+    while True:
+        if len(g_conn_pool) < thread_max:
+            client, _ = g_socket_server.accept()  # 阻塞，等待客户端连接
+            # 加入连接池
+            g_conn_pool.append(client)
+            # 给每个客户端创建一个独立的线程进行管理
+            thread = threading.Thread(target=message_handle, args=(client,))
+            # 设置成守护线程
+            thread.setDaemon(True)
+            thread.start()
+        else:
+            time.sleep(0.1)
+ 
+ 
+def message_handle(client):
+    """
+    消息处理
+    """
+    while True:
+        bytes = client.recv(1024)
+        print("客户端消息:", bytes.decode(encoding='utf8'))
+        wordList = BytesWrite.SelectWords(pathSam)
+        BytesWrite.BytesWrite(wordList, pathDat)
+        msg = SearchWords(bytes)
+        client.sendall(msg.encode(encoding='utf8'))
+        if len(bytes) == 0:
+            client.close()
+            # 删除连接
+            g_conn_pool.remove(client)
+            print("有一个客户端下线了。")
+            break
 
 class MyServer(socketserver.BaseRequestHandler):
     def handle(self):
@@ -37,7 +87,7 @@ class MyServer(socketserver.BaseRequestHandler):
 def SearchWords(data):
     wList = data.split()
     l = open(pathLog, 'a+')
-    fd = os.open(pathDat, os.O_RDWR)
+    fd = os.open(pathDat, os.O_RDONLY)
     m = mmap.mmap(fd, 0,  access= mmap.ACCESS_READ)
     rst = ''
     for i in range(0, len(wList)):
@@ -86,7 +136,28 @@ def draw():
     plt.show()
 
 if __name__ == "__main__":
-    s = socketserver.ThreadingTCPServer(ip_port,MyServer)
-    s.serve_forever()
+    #s = socketserver.ThreadingTCPServer(ip_port,MyServer)
+    #s.serve_forever()
 
-    draw()
+    #draw()
+    init()
+    # 新开一个线程，用于接收新连接
+    thread = threading.Thread(target=accept_client)
+    thread.setDaemon(True)
+    thread.start()
+    # 主线程逻辑
+    while True:
+        cmd = input("""--------------------------
+输入1:查看当前在线人数
+输入2:给指定客户端发送消息
+输入3:关闭服务端
+""")
+        if cmd == '1':
+            print("--------------------------")
+            print("当前在线人数：", len(g_conn_pool))
+        elif cmd == '2':
+            print("--------------------------")
+            index, msg = input("请输入“索引,消息”的形式：").split(",")
+            g_conn_pool[int(index)].sendall(msg.encode(encoding='utf8'))
+        elif cmd == '3':
+            exit()
